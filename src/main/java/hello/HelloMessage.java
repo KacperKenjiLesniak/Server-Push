393a680a820c430f2bf19847f319c499cc003a26
KacperKenjiLesniak <kenjikacper@gmail.com>
2017-10-25 15:27:58 +0200

    Initial commit. Java basic server using Spring and STOMP

---
+++ src/main/java/hello/HelloMessage.java
@@ -0,0 +1,23 @@
+package hello;
+
+/**
+ * Created by kenjik on 25.10.17.
+ */
+public class HelloMessage {
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
